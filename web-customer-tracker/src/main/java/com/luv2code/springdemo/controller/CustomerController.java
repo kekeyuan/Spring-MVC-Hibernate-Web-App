b31c26fdaf887b2f9204f9333d5d050f8da56b98
Ke Yuan <yubaoxiaolin@gmail.com>
2023-02-11 22:28:40 +0200

    update customer column

--- web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
@@ -46,11 +47,25 @@ public class CustomerController {
 	
 	@PostMapping("/saveCustomer")
 	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
-		customerService.saveCustomer(theCustomer);	
-		
-		
-		
+		customerService.saveCustomer(theCustomer);			
 		return"redirect:/customer/list";
+	}
+	
+	
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("CustomerId")int theId,Model theModel) {
+		
+		//get the customer from the service
+		Customer theCustomer=customerService.getCustomer(theId);
+		
+		//set customer as a model attribute to pre-populate the form
+		theModel.addAttribute("customer",theCustomer);
+		
+		
+		//send over to our form
+		return "customer-form";		
+	
 	}
 }
 	
--- web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -7,7 +7,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import com.luv2code.springdemo.entity.Customer;
 
@@ -41,7 +41,18 @@ public class CustomerDAOImpl implements CustomerDAO {
 		Session currentSession=sessionFactory.getCurrentSession();
 		
 		//save the customer...finally LOL
-		currentSession.save(theCustomer);	
+		currentSession.saveOrUpdate(theCustomer);	
+
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		//get the current hibernate session
+		Session currentSession=sessionFactory.getCurrentSession();
+		
+		//now retrieve read from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		return theCustomer;
 
 	}
 
